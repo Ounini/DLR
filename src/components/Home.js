7307037daf8c2d45e1890eda70e01153ddd0f768
Ounini <wittyways1216@gmail.com>
2025-05-07 23:11:38 +0100

    Update index

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const caroData = [
     img: pic,
     text: "Welcome to Divine Light Of Resurrection ",
     alt: "welcome",
-    phase: '"A Place of Hope, Love, and Worship."',
+    phase: '"A Place of Hope, Love, and Recovery."',
     joinText: "Join us live",
     joinUrl: "/live",
     worship: "Worship with us",
